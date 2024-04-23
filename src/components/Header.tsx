d4ecd79c445d1793963e3c42d156e7e86c4e1033
SeonnHo <prefer914@gmail.com>
2024-04-23 23:51:00 +0900

    헤더 컴포넌트 스타일 수정 및 로그인 버튼 UI 구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,23 @@
+'use client';
+
 import Link from 'next/link';
+import { Button } from './ui/button';
 
 export default function Header() {
   return (
-    <header className="w-full h-[60px] lg:h-[80px] sticky top-0 left-0">
-      <div className="flex items-center h-full lg:max-w-screen-lg mx-auto">
-        <Link href={'/'}>
-          <h1 className="font-bold text-xl max-lg:ml-4">FIT ME UP</h1>
-        </Link>
-      </div>
-    </header>
+    <>
+      <header className="w-full h-[60px] lg:h-[80px] fixed top-0 left-0 z-50 bg-white">
+        <div className="flex items-center h-full lg:max-w-screen-lg mx-auto">
+          <Link href={'/'}>
+            <h1 className="font-bold text-xl max-lg:ml-4">FIT ME UP</h1>
+          </Link>
+          <div className="ml-auto mr-2">
+            <Link href={'/sign-in'}>
+              <Button variant={'default'}>로그인</Button>
+            </Link>
+          </div>
+        </div>
+      </header>
+    </>
   );
 }
