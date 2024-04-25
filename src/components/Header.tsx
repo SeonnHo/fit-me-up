96cd97cd730ee16f1397db9a16452d714bf3e1e6
SeonnHo <prefer914@gmail.com>
2024-04-25 20:12:59 +0900

    공통 헤더 컴포넌트 세션 유무에 따른 UI 변경 부분 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,8 +2,12 @@
 
 import Link from 'next/link';
 import { Button } from './ui/button';
+import { signOut, useSession } from 'next-auth/react';
 
 export default function Header() {
+  const { data: session } = useSession();
+  console.log('mount');
+
   return (
     <>
       <header className="w-full h-[60px] lg:h-[80px] fixed top-0 left-0 z-50 bg-white">
@@ -12,9 +16,20 @@ export default function Header() {
             <h1 className="font-bold text-xl max-lg:ml-4">FIT ME UP</h1>
           </Link>
           <div className="ml-auto mr-2">
-            <Link href={'/sign-in'}>
-              <Button variant={'default'}>로그인</Button>
-            </Link>
+            {session && session.user ? (
+              <div className="flex items-center">
+                <p className="text-sm font-bold mr-4">
+                  {session.user.nickname}
+                </p>
+                <Button variant={'default'} onClick={() => signOut()}>
+                  로그아웃
+                </Button>
+              </div>
+            ) : (
+              <Link href={'/api/auth/signin'}>
+                <Button variant={'default'}>로그인</Button>
+              </Link>
+            )}
           </div>
         </div>
       </header>
