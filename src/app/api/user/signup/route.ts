bbeb1da5c27387339fb91a005ac9a1c0d60e832e
SeonnHo <prefer914@gmail.com>
2024-04-25 19:01:06 +0900

    회원가입 API 구현

---
+++ src/app/api/user/signup/route.ts
@@ -0,0 +1,27 @@
+import { connectDB } from '@/lib/database';
+import * as bcrypt from 'bcryptjs';
+import { NextResponse } from 'next/server';
+
+interface User {
+  email: string;
+  password: string;
+  nickname: string;
+}
+
+export async function POST(request: Request) {
+  const database = connectDB.db('fit_me_up');
+  const usersCollection = database.collection<User>('users');
+
+  const body: User = await request.json();
+
+  const saltRounds = 10;
+  const salt = await bcrypt.genSalt(saltRounds);
+
+  const user = await usersCollection.insertOne({
+    email: body.email,
+    password: await bcrypt.hash(body.password, salt),
+    nickname: body.nickname,
+  });
+
+  return NextResponse.json(user);
+}
