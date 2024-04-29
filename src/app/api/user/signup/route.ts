87adc5293881a4e908c4a617d49749d0b3043795
SeonnHo <prefer914@gmail.com>
2024-04-30 00:08:10 +0900

    일반 회원가입 API 유저 인터페이스에 type 속성 추가

--- src/app/api/user/signup/route.ts
+++ src/app/api/user/signup/route.ts
@@ -6,6 +6,7 @@ interface User {
   email: string;
   password: string;
   nickname: string;
+  type: string;
 }
 
 export async function POST(request: Request) {
@@ -13,6 +14,7 @@ export async function POST(request: Request) {
   const usersCollection = database.collection<User>('users');
 
   const body: User = await request.json();
+  const TYPE = 'credentials';
 
   const saltRounds = 10;
   const salt = await bcrypt.genSalt(saltRounds);
@@ -21,6 +23,7 @@ export async function POST(request: Request) {
     email: body.email,
     password: await bcrypt.hash(body.password, salt),
     nickname: body.nickname,
+    type: TYPE,
   });
 
   return NextResponse.json(user);
