56ffa859e1978140df589edea7ea25ad601d8fc7
SeonnHo <prefer914@gmail.com>
2024-04-30 00:11:53 +0900

    처음 소셜 로그인 시 닉네임 설정을 위한 API 구현

---
+++ src/app/api/user/signup/oauth/route.ts
@@ -0,0 +1,31 @@
+import { connectDB } from '@/lib/database';
+import { NextRequest, NextResponse } from 'next/server';
+
+interface RequestBody {
+  id: string;
+  nickname: string;
+}
+
+interface User {
+  _id: string;
+  email: string | null | undefined;
+  password: string | null | undefined;
+  nickname: string;
+  type: string;
+}
+
+export async function POST(reqeust: NextRequest) {
+  const database = connectDB.db('fit_me_up');
+  const usersCollection = database.collection<User>('users');
+
+  const body: RequestBody = await reqeust.json();
+
+  const result = await usersCollection.updateOne(
+    {
+      _id: body.id,
+    },
+    { $set: { nickname: body.nickname } }
+  );
+
+  return NextResponse.json(result);
+}
