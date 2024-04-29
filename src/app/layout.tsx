86bb2187d5d0d15c1e587aab2ca2f16ddefd7379
SeonnHo <prefer914@gmail.com>
2024-04-29 23:57:06 +0900

    root layout viewport 설정 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { Nanum_Gothic } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/Header';
 import AuthProvider from '@/context/AuthContext';
+import { Toaster } from '@/components/ui/toaster';
 
 const nanumGothic = Nanum_Gothic({
   weight: ['400', '700', '800'],
@@ -11,6 +12,12 @@ const nanumGothic = Nanum_Gothic({
 
 export const metadata: Metadata = {
   title: 'Fit Me Up',
+};
+
+export const viewport: Viewport = {
+  initialScale: 1,
+  width: 'device-width',
+  userScalable: false,
 };
 
 export default function RootLayout({
@@ -24,6 +31,7 @@ export default function RootLayout({
         <AuthProvider>
           <Header />
           {children}
+          <Toaster />
         </AuthProvider>
       </body>
     </html>
