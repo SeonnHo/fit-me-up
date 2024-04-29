54b2a35454b8f80a4cad47e9b7f330714e5180cf
SeonnHo <prefer914@gmail.com>
2024-04-30 00:05:27 +0900

    닉네임 중복 확인 API 구현

---
+++ src/app/api/user/check/nickname/route.ts
@@ -0,0 +1,27 @@
+import { connectDB } from '@/lib/database';
+import { NextRequest, NextResponse } from 'next/server';
+
+interface User {
+  _id: string;
+  email: string | null | undefined;
+  password: string | null | undefined;
+  nickname: string;
+  type: string;
+}
+
+export async function POST(request: NextRequest) {
+  const database = connectDB.db('fit_me_up');
+  const usersCollection = database.collection<User>('users');
+
+  const nickname: string = await request.json();
+
+  const user = await usersCollection.findOne({
+    nickname: nickname,
+  });
+
+  if (user) {
+    return NextResponse.json(true);
+  }
+
+  return NextResponse.json(false);
+}
