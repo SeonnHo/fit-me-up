792d258437d3745328ef60a31d630726441f1a15
SeonnHo <prefer914@gmail.com>
2024-05-01 20:39:03 +0900

    Roboto 폰트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata, Viewport } from 'next';
-import { Nanum_Gothic } from 'next/font/google';
+import { Nanum_Gothic, Roboto } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/Header';
 import AuthProvider from '@/context/AuthContext';
@@ -8,6 +8,12 @@ import { Toaster } from '@/components/ui/toaster';
 const nanumGothic = Nanum_Gothic({
   weight: ['400', '700', '800'],
   subsets: ['latin'],
+});
+
+const roboto = Roboto({
+  weight: '500',
+  subsets: ['latin'],
+  variable: '--Roboto',
 });
 
 export const metadata: Metadata = {
@@ -27,7 +33,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={nanumGothic.className}>
+      <body className={`${nanumGothic.className} ${roboto.variable}`}>
         <AuthProvider>
           <Header />
           {children}
