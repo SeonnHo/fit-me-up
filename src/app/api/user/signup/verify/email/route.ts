3d191b14ea31bdbcb581a6f7c722b448af40ea37
SeonnHo <prefer914@gmail.com>
2024-05-03 23:02:13 +0900

    인증 번호 메일 발송 API 구현

---
+++ src/app/api/user/signup/verify/email/route.ts
@@ -0,0 +1,15 @@
+import { sendEmail } from '@/lib/mailer';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(request: NextRequest) {
+  const email: string = await request.json();
+
+  const randomNumber = String(Math.floor(Math.random() * 888889) + 111111);
+
+  const mailerInfo = await sendEmail({
+    email: email,
+    authNumber: randomNumber,
+  });
+
+  return NextResponse.json({ ...mailerInfo, authNumber: randomNumber });
+}
