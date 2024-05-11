55c6d76caa4ea2044e7113f865964746c1b9f237
SeonnHo <prefer914@gmail.com>
2024-05-11 18:37:04 +0900

    nodemailer 모듈 html 수정

--- src/lib/mailer.ts
+++ src/lib/mailer.ts
@@ -23,51 +23,45 @@ const transporter = nodemailer.createTransport({
 });
 
 export const sendEmail = async ({ email, authNumber }: MailOptions) => {
-  const authNumberList = authNumber.split('');
-
   const mailOptions = {
     from: process.env.NODEMAILER_USER,
     to: email,
     subject: '[핏미업] 회원가입을 위한 이메일 인증 번호입니다.',
-    html: `<div
-    style='
-    text-align: center;
-    width: 700px;
-    height: 100%;
-    margin: 1rem auto;
-    padding: 20px;
-    box-shadow: 1px 1px 3px 0px #eee
-    '>
-    <h2>핏미업 회원가입 인증 번호</h2>
-    <div style='height: 1px; background-color: #eee; margin-bottom: 2rem;'/>
-    <p>회원가입을 위한 이메일 인증 번호입니다. 아래 번호를 인증 번호 입력란에 입력해주세요.</p>
-    <div style='height: 1px; background-color: #eee; margin-top: 2rem; margin-bottom: 2rem;'/>
-    <div style='display: flex; justify-content: center; align-items: center;'>
-      <p style='width: 50px; height: 60px; background-color: #eee; border-radius: 8px; text-align: center; font-size: 2rem; font-weight: 700; padding: 10px; line-height: 60px;'>
-        ${authNumberList[0]}
-      </p>
-      <p style='width: 50px; height: 60px; background-color: #eee; border-radius: 8px; text-align: center; font-size: 2rem; font-weight: 700; padding: 10px; line-height: 60px; margin-left: 0.5rem; margin-right: 0.5rem;'>
-        ${authNumberList[1]}
-      </p>
-      <p style='width: 50px; height: 60px; background-color: #eee; border-radius: 8px; text-align: center; font-size: 2rem; font-weight: 700; padding: 10px; line-height: 60px; margin-right: 0.5rem;'>
-        ${authNumberList[2]}
-      </p>
-      <p style='width: 50px; height: 60px; background-color: #eee; border-radius: 8px; text-align: center; font-size: 2rem; font-weight: 700; padding: 10px; line-height: 60px;'>
-        ${authNumberList[3]}
-      </p>
-      <p style='width: 50px; height: 60px; background-color: #eee; border-radius: 8px; text-align: center; font-size: 2rem; font-weight: 700; padding: 10px; line-height: 60px; margin-left: 0.5rem; margin-right: 0.5rem;'>
-        ${authNumberList[4]}
-      </p>
-      <p style='width: 50px; height: 60px; background-color: #eee; border-radius: 8px; text-align: center; font-size: 2rem; font-weight: 700; padding: 10px; line-height: 60px;'>
-        ${authNumberList[5]}
-      </p>
-    </div>
-    <div style='height: 1px; background-color: #eee; margin-top: 2rem; margin-bottom: 2rem;'/>
-    <p style='color: #666; font-size: 12px;'>
-    이 메일은 발신 전용으로, 회신을 통해 문의하시는 경우 답변을 드릴 수 없습니다.<br/>
-    자세한 사항은 핏미업 문의게시판을 이용해 주시기 바랍니다.
-    </p>
-    </div>`,
+    html: `<table border="0" cellpadding="0" cellspacing="0" width="100%">
+    <tbody>
+      <tr>
+        <td align="center">
+          <table border="0" cellpadding="0" cellspacing="0" width="600">
+            <tbody>
+              <tr>
+                <td align="center" style="padding-top: 10px; padding-bottom: 10px; padding-left: 0; padding-right: 0; font-size: 1.25rem">
+                  <b>핏미업 회원가입 이메일 인증 코드</b>
+                </td>
+              </tr>
+              <tr>
+                <td align="center" style="padding-top: 10px; padding-bottom: 20px; padding-left: 0; padding-right: 0;">
+                  회원가입을 위한 이메일 인증 코드입니다. 아래 코드를 인증 코드 입력란에 입력해 주세요.
+                </td>
+              </tr>
+              <tr>
+                <td align="center" style="border-top: 1px solid #EEEEEE; border-bottom: 1px solid #EEEEEE;">
+                  <table border="0" cellpadding="0" cellspacing="0" width="300" style="padding-top: 100px; padding-bottom: 100px; padding-left: 0; padding-right: 0;">
+                    <tbody>
+                      <tr>
+                        <td align="center" bgColor="#EEEEEE" style="border-radius: 8px; font-size: 2rem; padding-top: 10px; padding-bottom: 10px; padding-left: 0; padding-right: 0;">
+                          <b>${authNumber}</b>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </td>
+      </tr>
+    </tbody>
+    </table>`,
   };
 
   const info = await transporter.sendMail(mailOptions);
