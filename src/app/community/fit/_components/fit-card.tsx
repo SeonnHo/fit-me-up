75668ea768bbe81018fd3afa2ab247af191137f1
SeonnHo <prefer914@gmail.com>
2024-05-23 21:41:57 +0900

    FitCard 컴포넌트 상세보기 모달창 UI 구현

--- src/app/community/fit/_components/fit-card.tsx
+++ src/app/community/fit/_components/fit-card.tsx
@@ -1,3 +1,13 @@
+'use client';
+
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from '@/components/ui/alert-dialog';
 import {
   Card,
   CardContent,
@@ -5,9 +15,12 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import { Divide } from 'lucide-react';
 import Image from 'next/image';
+import { useState } from 'react';
 import { BiCommentDetail } from 'react-icons/bi';
-import { FaHeart } from 'react-icons/fa';
+import { FaHeart, FaRegComment, FaRegHeart } from 'react-icons/fa';
+import { MdClose } from 'react-icons/md';
 
 interface Props {
   title: string;
@@ -28,32 +41,96 @@ export default function FitCard({
   commentCount,
   likeCount,
 }: Props) {
+  const [isOpenDetails, setIsOpenDetails] = useState(false);
+  const [isLike, setIsLike] = useState(false);
+  const [isShowComment, setIsShowComment] = useState(false);
+
   return (
-    <Card className="transition duration-200 hover:scale-105 cursor-pointer">
-      <CardHeader>
-        <CardTitle>{title}</CardTitle>
-        <CardDescription>{description}</CardDescription>
-      </CardHeader>
-      <CardContent className="space-y-2">
-        <Image
-          src={image}
-          alt={alt}
-          width={300}
-          height={400}
-          className="w-full"
-        />
-        <p>{content}</p>
-        <div className="flex justify-end space-x-4">
-          <div className="flex items-center space-x-1">
-            <BiCommentDetail className="size-5" />
-            <p>{commentCount}</p>
+    <>
+      <Card
+        className="transition duration-200 hover:scale-105 cursor-pointer w-[300px]"
+        onClick={() => setIsOpenDetails(true)}
+      >
+        <CardHeader>
+          <CardTitle className="text-lg line-clamp-1">{title}</CardTitle>
+          <CardDescription>{description}</CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-2">
+          <Image
+            src={image}
+            alt={alt}
+            width={300}
+            height={400}
+            className="w-full"
+          />
+          <p className="line-clamp-1 text-sm">{content}</p>
+          <div className="flex justify-end space-x-4">
+            <div className="flex items-center space-x-1">
+              <FaHeart className="size-5 text-red-500" />
+              <p className="text-sm">{likeCount}</p>
+            </div>
+            <div className="flex items-center space-x-1">
+              <BiCommentDetail className="size-5" />
+              <p className="text-sm">{commentCount}</p>
+            </div>
           </div>
-          <div className="flex items-center space-x-1">
-            <FaHeart className="size-5 text-red-500" />
-            <p>{likeCount}</p>
-          </div>
-        </div>
-      </CardContent>
-    </Card>
+        </CardContent>
+      </Card>
+
+      <AlertDialog open={isOpenDetails} onOpenChange={setIsOpenDetails}>
+        <AlertDialogContent
+          onEscapeKeyDown={(e) => e.preventDefault()}
+          className="max-md:h-full max-md:flex-grow overflow-hidden"
+        >
+          <AlertDialogCancel className="absolute top-2 right-2 border-none p-0 m-0 h-auto w-auto bg-transparent hover:bg-transparent">
+            <MdClose className="size-6 text-zinc-400 hover:text-zinc-700" />
+          </AlertDialogCancel>
+
+          <AlertDialogHeader>
+            <AlertDialogTitle>{title}</AlertDialogTitle>
+            <AlertDialogDescription>{description}</AlertDialogDescription>
+          </AlertDialogHeader>
+          {isShowComment ? (
+            <div
+              className="bg-red-500 h-[600px] animate-slide-left"
+              onClick={() => setIsShowComment(false)}
+            ></div>
+          ) : (
+            <div className="md:h-[600px] overflow-y-scroll scrollbar-hide flex flex-col gap-2">
+              <Image
+                src={image}
+                alt={alt}
+                width={300}
+                height={400}
+                className="w-full"
+              />
+              <div className="flex justify-between">
+                <p className="text-sm font-bold">
+                  좋아요 {likeCount}개 • 댓글 {commentCount}개
+                </p>
+                <div className="flex items-center gap-4">
+                  {isLike ? (
+                    <FaHeart
+                      className="size-6 text-red-500 cursor-pointer animate-heart-race"
+                      onClick={() => setIsLike(false)}
+                    />
+                  ) : (
+                    <FaRegHeart
+                      className="size-6 cursor-pointer"
+                      onClick={() => setIsLike(true)}
+                    />
+                  )}
+                  <FaRegComment
+                    className="size-6 cursor-pointer"
+                    onClick={() => setIsShowComment(true)}
+                  />
+                </div>
+              </div>
+              <p className="text-sm">{content}</p>
+            </div>
+          )}
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
   );
 }
