22f3181b3d93e13a202c235b521d40daecab1d11
SeonnHo <prefer914@gmail.com>
2024-06-01 23:58:48 +0900

    FitCard 컴포넌트에 사용되는, 이미지와 해당 이미지에 보이는 옷에 대한 정보가 기재된 FitImage 컴포넌트 UI 구현

---
+++ src/app/community/fit/_components/fit-image.tsx
@@ -0,0 +1,79 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import { useState } from 'react';
+import { MdOutlineInfo, MdOutlineImage } from 'react-icons/md';
+
+interface Props {
+  image: string;
+  fitInfo?: {
+    category: string;
+    value: string;
+  }[];
+}
+
+export default function FitImage({ image, fitInfo }: Props) {
+  const [isShowInfo, setIsShowInfo] = useState(false);
+
+  return (
+    <div className="relative">
+      {isShowInfo ? (
+        <div className="flex flex-col h-[555px] max-sm:h-[408px]">
+          <div className="flex justify-end">
+            <Button
+              variant="outline"
+              type="button"
+              className="text-sm flex justify-center items-center px-2 py-1 h-auto space-x-1 mt-2 mr-2"
+              onClick={() => setIsShowInfo(false)}
+            >
+              <MdOutlineImage />
+              <p className="text-xs">이미지</p>
+            </Button>
+          </div>
+          {fitInfo ? (
+            <div className="p-2">
+              <table className="w-full">
+                <tbody>
+                  {fitInfo.map((info) => (
+                    <tr key={info.category} className="border text-center">
+                      <td className="border-r w-1/5 font-bold bg-zinc-100">
+                        {info.category}
+                      </td>
+                      <td>{info.value}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          ) : (
+            <div className="flex justify-center items-center flex-grow">
+              <p className="font-bold text-center">
+                해당 핏에 대한 정보가 없습니다.
+              </p>
+            </div>
+          )}
+        </div>
+      ) : (
+        <>
+          <Image
+            src={image}
+            alt="핏이미지"
+            width={300}
+            height={400}
+            className="w-full"
+          />
+          <Button
+            variant="outline"
+            type="button"
+            className="text-sm flex justify-center items-center absolute top-2 right-2 px-2 py-1 h-auto space-x-1"
+            onClick={() => setIsShowInfo(true)}
+          >
+            <MdOutlineInfo />
+            <p className="text-xs">핏 정보</p>
+          </Button>
+        </>
+      )}
+    </div>
+  );
+}
