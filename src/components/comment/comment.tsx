b8f9f12bb8460b288d88dab9552528e2b2068e19
SeonnHo <prefer914@gmail.com>
2024-06-02 23:56:40 +0900

    Comment 컴포넌트 수정, 삭제, 답글 달기 버튼 UI 구현

--- src/components/comment/comment.tsx
+++ src/components/comment/comment.tsx
@@ -1,4 +1,8 @@
+'use client';
+
+import { useSession } from 'next-auth/react';
 import Image from 'next/image';
+import { Button } from '../ui/button';
 
 interface Props {
   image: string;
@@ -8,6 +12,8 @@ interface Props {
 }
 
 export default function Comment({ image, nickname, createAt, content }: Props) {
+  const { data: session } = useSession();
+
   return (
     <li className="flex space-x-2 items-start border-y py-2 [&:first-child~li]:border-t-0">
       <Image
@@ -17,12 +23,39 @@ export default function Comment({ image, nickname, createAt, content }: Props) {
         height={40}
         className="rounded-full flex-grow-0 shrink-0"
       />
-      <div className="flex flex-col space-y-2">
-        <div className="flex space-x-2 items-center">
-          <p className="text-sm font-bold">{nickname}</p>
-          <p className="text-xs text-zinc-400">{createAt}</p>
+      <div className="w-full flex flex-col items-start space-y-2">
+        <div className="w-full flex justify-between items-center">
+          <div className="flex space-x-2 items-center">
+            <p className="text-sm font-bold">{nickname}</p>
+            <p className="text-xs text-zinc-400">{createAt}</p>
+          </div>
+          {session && nickname === session.user.nickname && (
+            <div className="flex space-x-2 items-center">
+              <Button
+                variant="ghost"
+                type="button"
+                className="text-xs text-zinc-400 font-bold h-auto p-0 hover:bg-transparent"
+              >
+                수정
+              </Button>
+              <Button
+                variant="ghost"
+                type="button"
+                className="text-xs text-zinc-400 font-bold h-auto p-0 hover:bg-transparent"
+              >
+                삭제
+              </Button>
+            </div>
+          )}
         </div>
         <p className="text-sm">{content}</p>
+        <Button
+          variant="ghost"
+          type="button"
+          className="text-xs text-zinc-400 font-bold h-auto p-0 hover:bg-transparent"
+        >
+          답글 달기
+        </Button>
       </div>
     </li>
   );
