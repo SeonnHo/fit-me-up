6b454e3381c0b3b85c311cb89cb87a5751556ee1
SeonnHo <prefer914@gmail.com>
2024-07-02 22:21:44 +0900

    fit-card-list 컴포넌트 mock data에 user 속성 추가

--- src/app/community/fit/_components/fit-card-list.tsx
+++ src/app/community/fit/_components/fit-card-list.tsx
@@ -10,6 +10,10 @@ export default function FitCardList() {
       content: '따스해보이지 않나요? ㅎㅎ',
       commentCount: '6',
       likeCount: '5',
+      user: {
+        _id: '3453523720',
+        nickname: '노야',
+      },
     },
     {
       id: 2,
@@ -19,6 +23,10 @@ export default function FitCardList() {
       content: '이런 날씨에 입으려고 이걸 샀지롱',
       commentCount: '0',
       likeCount: '5',
+      user: {
+        _id: '3453523720',
+        nickname: '왕족발',
+      },
     },
     {
       id: 3,
@@ -28,6 +36,10 @@ export default function FitCardList() {
       content: '오늘의 착장',
       commentCount: '2',
       likeCount: '3',
+      user: {
+        _id: '3453523720',
+        nickname: '장충동',
+      },
     },
     {
       id: 4,
@@ -37,6 +49,10 @@ export default function FitCardList() {
       content: '이번에 새로 산건데 저한테 맞는지 잘 모르겠네요... ㅎㅎㅎ',
       commentCount: '4',
       likeCount: '3',
+      user: {
+        _id: '3453523720',
+        nickname: '미요네즈',
+      },
     },
     {
       id: 5,
@@ -46,11 +62,15 @@ export default function FitCardList() {
       content: '잇 이즈 투데이 룩 히히',
       commentCount: '1',
       likeCount: '2',
+      user: {
+        _id: '3453523720',
+        nickname: '선호등',
+      },
     },
   ];
 
   return (
-    <section className="grid grid-cols-4 grid-flow-row gap-4 my-5 max-xl:grid-cols-3 max-lg:grid-cols-2 max-md:flex max-md:flex-col max-md:gap-5">
+    <section className="grid grid-cols-3 grid-flow-row gap-4 my-5 max-xl:grid-cols-2 max-lg:flex max-lg:flex-col">
       {cardItemlist.map((item) => (
         <FitCard key={item.id} {...item} />
       ))}
