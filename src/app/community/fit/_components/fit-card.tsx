b4a48417c97b6cccc26610325f3b2a9afb6f3020
SeonnHo <prefer914@gmail.com>
2024-07-04 20:45:47 +0900

    핏 카드 컴포넌트 모바일 화면일 때 댓글창이 Dialog가 아닌 Drawer로 보이도록 UI 수정 및 날짜 포맷 구현

--- src/app/community/fit/_components/fit-card.tsx
+++ src/app/community/fit/_components/fit-card.tsx
@@ -5,6 +5,7 @@ import {
   AlertDialog,
   AlertDialogCancel,
   AlertDialogContent,
+  AlertDialogDescription,
   AlertDialogHeader,
   AlertDialogTitle,
 } from '@/components/ui/alert-dialog';
@@ -25,10 +26,18 @@ import FitImage from './fit-image';
 import { useCommentStore } from '@/store/use-comment-store';
 import { useSession } from 'next-auth/react';
 import { toast } from '@/components/ui/use-toast';
+import { useMediaQuery } from '@/hooks/use-media-query';
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+} from '@/components/ui/drawer';
+import Image from 'next/image';
 
 interface Props {
-  title: string;
-  description: string;
+  createAt: Date;
   content: string;
   image: string;
   commentCount: string;
@@ -40,20 +49,19 @@ interface Props {
 }
 
 export default function FitCard({
-  title,
-  description,
+  createAt,
   content,
   image,
   commentCount,
   likeCount,
   user,
 }: Props) {
-  // const [isOpenDetails, setIsOpenDetails] = useState(false);
   const [isLike, setIsLike] = useState(false);
   const [isShowComment, setIsShowComment] = useState(false);
   const [comment, setComment] = useState('');
   const { mentionedUser, setMentionedUser } = useCommentStore();
   const { data: session } = useSession();
+  const isMobile = useMediaQuery('(max-width: 768px)');
 
   const handleAddComment = async () => {
     if (!comment) {
@@ -67,12 +75,47 @@ export default function FitCard({
     // TODO: 댓글 추가 요청 API 로직 작성
   };
 
+  const dateFormatter = (date: Date): string => {
+    const milliSeconds = new Date().getTime() - date.getTime();
+    const seconds = milliSeconds / 1000;
+    if (seconds < 60) return `방금 전`;
+    const minutes = seconds / 60;
+    if (minutes < 60) return `${Math.floor(minutes)}분 전`;
+    const hours = minutes / 60;
+    if (hours < 24) return `${Math.floor(hours)}시간 전`;
+    const days = hours / 24;
+    if (days < 7) return `${Math.floor(days)}일 전`;
+    const weeks = days / 7;
+    if (weeks < 5) return `${Math.floor(weeks)}주 전`;
+    const months = days / 30;
+    if (months < 12) return `${Math.floor(months)}개월 전`;
+    // const years = days / 365;
+    // return `${Math.floor(years)}년 전`;
+
+    const createAt = new Date(
+      date.toLocaleString('en-US', { timeZone: 'Asia/Seoul' })
+    );
+    return `${createAt.getFullYear()}년 ${
+      createAt.getMonth() + 1
+    }월 ${createAt.getDate()}일`;
+  };
+
   return (
     <>
       <Card className="w-[400px] max-sm:w-full max-sm:rounded-none max-sm:border-y max-sm:border-x-0">
-        <CardHeader className="p-3">
-          <CardTitle className="text-lg line-clamp-1">{title}</CardTitle>
-          <CardDescription>{description}</CardDescription>
+        <CardHeader className="p-3 flex flex-row justify-between items-center space-y-0">
+          <div className="flex space-x-2 items-center">
+            <Image
+              src="/profile_icon.png"
+              alt="profile"
+              width={40}
+              height={40}
+            />
+            <CardTitle className="text-base line-clamp-1">
+              {user.nickname}
+            </CardTitle>
+          </div>
+          <CardDescription>{dateFormatter(createAt)}</CardDescription>
         </CardHeader>
         <CardContent className="space-y-2 p-0">
           <div className="relative w-full h-[500px]">
@@ -108,117 +151,232 @@ export default function FitCard({
         </CardContent>
       </Card>
 
-      <AlertDialog
-        open={isShowComment}
-        onOpenChange={(open) => setIsShowComment(open)}
-      >
-        <AlertDialogContent
-          onEscapeKeyDown={(e) => e.preventDefault()}
-          className="max-sm:max-w-full p-3 gap-0 max-sm:data-[state=open]:duration-300 max-sm:data-[state=closed]:duration-300 max-sm:translate-y-[0%] max-sm:bottom-[0%] max-sm:top-auto max-sm:data-[state=open]:slide-in-from-bottom-full max-sm:data-[state=open]:zoom-in-100 max-sm:data-[state=closed]:slide-out-to-bottom max-sm:data-[state=closed]:zoom-out-100"
+      {isMobile ? (
+        <Drawer
+          open={isShowComment}
+          onOpenChange={(open) => setIsShowComment(open)}
         >
-          <AlertDialogCancel className="absolute top-2 right-2 border-none p-0 m-0 h-auto w-auto bg-transparent hover:bg-transparent">
-            <MdClose className="size-6 text-zinc-400 hover:text-zinc-700" />
-          </AlertDialogCancel>
-
-          <AlertDialogHeader className="p-0">
-            <AlertDialogTitle className="text-base font-bold text-center">
-              댓글
-            </AlertDialogTitle>
-          </AlertDialogHeader>
-          <div className="flex flex-col min-h-[200px] max-h-[500px] sm:h-[600px] space-y-2 pt-5">
-            <ul className="overflow-scroll scrollbar-hide">
-              <Comment
-                image="/profile_icon.png"
-                nickname="good"
-                createAt="2024-06-01"
-                content="완전 멋있어요."
-              />
-              <Comment
-                image="/profile_icon.png"
-                nickname="good1"
-                createAt="2024-06-01"
-                content="완전 멋있어요."
-              />
-              <Comment
-                image="/profile_icon.png"
-                nickname="good2"
-                createAt="2024-06-01"
-                content="완전 멋있어요."
-              />
-              <Comment
-                image="/profile_icon.png"
-                nickname="good"
-                createAt="2024-06-01"
-                content="완전 멋있어요."
-              />
-              <Comment
-                image="/profile_icon.png"
-                nickname="good"
-                createAt="2024-06-01"
-                content="완전 멋있어요."
-              />
-              <Comment
-                image="/profile_icon.png"
-                nickname="good"
-                createAt="2024-06-01"
-                content="완전 멋있어요."
-              />
-              <Comment
-                image="/profile_icon.png"
-                nickname="good"
-                createAt="2024-06-01"
-                content="완전 멋있어요."
-              />
-              <Comment
-                image="/profile_icon.png"
-                nickname="good"
-                createAt="2024-06-01"
-                content="완전 멋있어요."
-              />
-              <Comment
-                image="/profile_icon.png"
-                nickname="good"
-                createAt="2024-06-01"
-                content="완전 멋있어요."
-              />
-            </ul>
-            <div className="flex flex-col space-y-1">
-              {mentionedUser && (
-                <div className="flex items-center space-x-4">
-                  <p className="text-xs font-bold">
-                    {mentionedUser}님에게 답글 추가...
-                  </p>
+          <DrawerContent onEscapeKeyDown={(e) => e.preventDefault()}>
+            <DrawerHeader className="p-2 gap-0">
+              <DrawerTitle className="text-base text-center">댓글</DrawerTitle>
+              <DrawerDescription className="text-center">
+                {session?.user ? '' : '로그인 후 이용 가능합니다.'}
+              </DrawerDescription>
+            </DrawerHeader>
+            <div className="flex flex-col min-h-[150px] max-h-[500px] space-y-2 px-2 pb-2">
+              <ul className="overflow-scroll">
+                <Comment
+                  image="/profile_icon.png"
+                  nickname="good"
+                  createAt="2024-06-01"
+                  content="완전 멋있어요."
+                />
+                <Comment
+                  image="/profile_icon.png"
+                  nickname="good"
+                  createAt="2024-06-01"
+                  content="완전 멋있어요."
+                />
+                <Comment
+                  image="/profile_icon.png"
+                  nickname="good"
+                  createAt="2024-06-01"
+                  content="완전 멋있어요."
+                />
+                <Comment
+                  image="/profile_icon.png"
+                  nickname="good"
+                  createAt="2024-06-01"
+                  content="완전 멋있어요."
+                />
+                <Comment
+                  image="/profile_icon.png"
+                  nickname="good"
+                  createAt="2024-06-01"
+                  content="완전 멋있어요."
+                />
+                <Comment
+                  image="/profile_icon.png"
+                  nickname="good"
+                  createAt="2024-06-01"
+                  content="완전 멋있어요."
+                />
+                <Comment
+                  image="/profile_icon.png"
+                  nickname="good"
+                  createAt="2024-06-01"
+                  content="완전 멋있어요."
+                />
+                <Comment
+                  image="/profile_icon.png"
+                  nickname="good"
+                  createAt="2024-06-01"
+                  content="완전 멋있어요."
+                />
+                <Comment
+                  image="/profile_icon.png"
+                  nickname="good"
+                  createAt="2024-06-01"
+                  content="완전 멋있어요."
+                />
+              </ul>
+              <div className="flex flex-col space-y-1">
+                {mentionedUser && (
+                  <div className="flex items-center space-x-4">
+                    <p className="text-xs font-bold">
+                      {mentionedUser}님에게 답글 추가...
+                    </p>
+                    <Button
+                      variant="ghost"
+                      type="button"
+                      className="text-xs text-zinc-400 font-bold h-auto p-0 hover:bg-transparent"
+                      onClick={() => setMentionedUser('')}
+                    >
+                      취소
+                    </Button>
+                  </div>
+                )}
+                <div className="flex space-x-2">
+                  <Input
+                    type="text"
+                    placeholder={
+                      mentionedUser ? '답글 추가...' : '댓글 추가...'
+                    }
+                    value={comment}
+                    onChange={(e) => setComment(e.target.value)}
+                    disabled={!session}
+                  />
                   <Button
-                    variant="ghost"
+                    variant="outline"
                     type="button"
-                    className="text-xs text-zinc-400 font-bold h-auto p-0 hover:bg-transparent"
-                    onClick={() => setMentionedUser('')}
+                    disabled={!comment}
+                    onClick={handleAddComment}
                   >
-                    취소
+                    <FaArrowUpLong className="size-5" />
                   </Button>
                 </div>
-              )}
-              <div className="flex space-x-2">
-                <Input
-                  type="text"
-                  placeholder="댓글 추가..."
-                  value={comment}
-                  onChange={(e) => setComment(e.target.value)}
-                  disabled={!session}
-                />
-                <Button
-                  variant="outline"
-                  type="button"
-                  disabled={!comment}
-                  onClick={handleAddComment}
-                >
-                  <FaArrowUpLong className="size-5" />
-                </Button>
               </div>
             </div>
-          </div>
-        </AlertDialogContent>
-      </AlertDialog>
+          </DrawerContent>
+        </Drawer>
+      ) : (
+        <AlertDialog
+          open={isShowComment}
+          onOpenChange={(open) => setIsShowComment(open)}
+        >
+          <AlertDialogContent
+            onEscapeKeyDown={(e) => e.preventDefault()}
+            className="p-3 gap-0"
+          >
+            <AlertDialogCancel className="absolute top-2 right-2 border-none p-0 m-0 h-auto w-auto bg-transparent hover:bg-transparent">
+              <MdClose className="size-6 text-zinc-400 hover:text-zinc-700" />
+            </AlertDialogCancel>
+
+            <AlertDialogHeader className="p-0 space-y-0">
+              <AlertDialogTitle className="text-base font-bold text-center">
+                댓글
+              </AlertDialogTitle>
+              <AlertDialogDescription className="text-center">
+                {session?.user ? '' : '로그인 후 이용 가능합니다.'}
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <div className="flex flex-col h-[600px] space-y-2 pt-2">
+              <ul className="overflow-scroll">
+                <Comment
+                  image="/profile_icon.png"
+                  nickname="good"
+                  createAt="2024-06-01"
+                  content="완전 멋있어요."
+                />
+                <Comment
+                  image="/profile_icon.png"
+                  nickname="good1"
+                  createAt="2024-06-01"
+                  content="완전 멋있어요."
+                />
+                <Comment
+                  image="/profile_icon.png"
+                  nickname="good2"
+                  createAt="2024-06-01"
+                  content="완전 멋있어요."
+                />
+                <Comment
+                  image="/profile_icon.png"
+                  nickname="good"
+                  createAt="2024-06-01"
+                  content="완전 멋있어요."
+                />
+                <Comment
+                  image="/profile_icon.png"
+                  nickname="good"
+                  createAt="2024-06-01"
+                  content="완전 멋있어요."
+                />
+                <Comment
+                  image="/profile_icon.png"
+                  nickname="good"
+                  createAt="2024-06-01"
+                  content="완전 멋있어요."
+                />
+                <Comment
+                  image="/profile_icon.png"
+                  nickname="good"
+                  createAt="2024-06-01"
+                  content="완전 멋있어요."
+                />
+                <Comment
+                  image="/profile_icon.png"
+                  nickname="good"
+                  createAt="2024-06-01"
+                  content="완전 멋있어요."
+                />
+                <Comment
+                  image="/profile_icon.png"
+                  nickname="good"
+                  createAt="2024-06-01"
+                  content="완전 멋있어요."
+                />
+              </ul>
+              <div className="flex flex-col space-y-1">
+                {mentionedUser && (
+                  <div className="flex items-center space-x-4">
+                    <p className="text-xs font-bold">
+                      {mentionedUser}님에게 답글 추가...
+                    </p>
+                    <Button
+                      variant="ghost"
+                      type="button"
+                      className="text-xs text-zinc-400 font-bold h-auto p-0 hover:bg-transparent"
+                      onClick={() => setMentionedUser('')}
+                    >
+                      취소
+                    </Button>
+                  </div>
+                )}
+                <div className="flex space-x-2">
+                  <Input
+                    type="text"
+                    placeholder={
+                      mentionedUser ? '답글 추가...' : '댓글 추가...'
+                    }
+                    value={comment}
+                    onChange={(e) => setComment(e.target.value)}
+                    disabled={!session}
+                  />
+                  <Button
+                    variant="outline"
+                    type="button"
+                    disabled={!comment}
+                    onClick={handleAddComment}
+                  >
+                    <FaArrowUpLong className="size-5" />
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </AlertDialogContent>
+        </AlertDialog>
+      )}
     </>
   );
 }
