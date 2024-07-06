c17bc2bfec742b8cc1b5a2292ee18f239653fa06
SeonnHo <prefer914@gmail.com>
2024-07-06 16:05:53 +0900

    next-auth.d.ts Session interface의 id 키 이름 _id -> id 수정

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -4,7 +4,7 @@ import NextAuth, { DefaultSession } from 'next-auth';
 declare module 'next-auth' {
   interface Session {
     user: {
-      _id?: string | null | undefined;
+      id?: string | null | undefined;
       email?: string | null | undefined;
       nickname?: string | null | undefined;
       accessToken?: string | null | undefined;
