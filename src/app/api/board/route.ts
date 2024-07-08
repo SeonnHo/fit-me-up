7fd267e30014714add5b2f3c1d71d0e15575cc2c
SeonnHo <prefer914@gmail.com>
2024-07-08 21:42:28 +0900

    게시글 등록 API 이미지 난수화 후 저장 및 게시글 요소에 likeCount, commentCount 추가

--- src/app/api/board/route.ts
+++ src/app/api/board/route.ts
@@ -2,13 +2,14 @@ import { connectDB } from '@/lib/database';
 import { NextRequest, NextResponse } from 'next/server';
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { InsertOneResult } from 'mongodb';
+import { v4 as uuidv4 } from 'uuid';
 
 interface RequsetBody {
   category: string;
-  title: string;
+  title?: string;
   content: string;
   user: string;
-  createAt: Date;
+  fitInfo?: { section: string; info: string }[];
 }
 
 const s3Client = new S3Client({ region: process.env.AWS_REGION });
@@ -20,22 +21,24 @@ export async function POST(request: NextRequest) {
   const formData = await request.formData();
 
   try {
-    const imageList = [];
+    const imageList: string[] = [];
 
     if (formData.has('image')) {
       for (const file of formData.getAll('image')) {
+        const uuid = uuidv4();
         if (file instanceof File) {
+          const fileExtension = `.${file.name.split('.').pop()}`;
           const arrayBuffer = await file.arrayBuffer();
           const buffer = Buffer.from(arrayBuffer);
           const params = {
             Bucket: process.env.AWS_BUCKET,
             Body: buffer,
-            Key: file.name,
+            Key: `board/${uuid + fileExtension}`,
             ContentType: file.type,
           };
           const command = new PutObjectCommand(params);
           await s3Client.send(command);
-          imageList.push(file.name);
+          imageList.push(uuid + fileExtension);
         }
       }
     }
@@ -48,6 +51,9 @@ export async function POST(request: NextRequest) {
       const body: RequsetBody = JSON.parse(text);
       result = await boardCollection.insertOne({
         ...body,
+        createAt: new Date(),
+        likeCount: 0,
+        commentCount: 0,
         images: imageList,
       });
     }
