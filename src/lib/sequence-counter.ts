8218db3fba03ac5e09a92cc210b495b516a5f1a8
SeonnHo <prefer914@gmail.com>
2024-08-06 23:30:41 +0900

    mongoDB에 자동 증가 id를 추가하기 위한 sequence-counter 모듈 구현

---
+++ src/lib/sequence-counter.ts
@@ -0,0 +1,24 @@
+import { connectDB } from './database';
+
+async function getNextSequence(name: string) {
+  const database = connectDB.db('fit_me_up');
+  const counterCollection = database.collection('counters');
+
+  const result = await counterCollection.findOneAndUpdate(
+    { sequenceName: name },
+    { $inc: { seq: 1 } },
+    { upsert: true, returnDocument: 'after' }
+  );
+
+  try {
+    if (result) {
+      return result.seq;
+    } else {
+      throw new Error('result 없음');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export { getNextSequence };
