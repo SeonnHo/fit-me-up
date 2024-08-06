83abd9cf7ace7494b08e9ae035ae9bd8afc4c3d4
SeonnHo <prefer914@gmail.com>
2024-08-06 23:22:31 +0900

    use-comment-store commentId 상태 추가 및 setCommentId 함수 추가

--- src/store/use-comment-store.ts
+++ src/store/use-comment-store.ts
@@ -1,15 +1,19 @@
 import { create } from 'zustand';
 
 interface Store {
+  commentId: string;
   mentionedUser: string;
   mentioningUser: string;
+  setCommentId: (id: string) => void;
   setMentionedUser: (user: string) => void;
   setMentioningUser: (user: string) => void;
 }
 
 export const useCommentStore = create<Store>((set) => ({
+  commentId: '',
   mentionedUser: '',
   mentioningUser: '',
+  setCommentId: (id) => set({ commentId: id }),
   setMentionedUser: (user) => set({ mentionedUser: user }),
   setMentioningUser: (user) => set({ mentioningUser: user }),
 }));
