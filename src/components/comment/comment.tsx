b010f44abc55cc33c09f78a8d4b70ebb6354b31d
SeonnHo <prefer914@gmail.com>
2024-08-06 23:19:30 +0900

    comment 컴포넌트 mention UI 추가 및 유저 정보가 담긴 useQuery 추가

--- src/components/comment/comment.tsx
+++ src/components/comment/comment.tsx
@@ -5,17 +5,43 @@ import Image from 'next/image';
 import { Button } from '../ui/button';
 import { useCommentStore } from '@/store/use-comment-store';
 import { toast } from '../ui/use-toast';
+import { dateFormatter } from '@/lib/date-formatter';
+import { useQuery } from '@tanstack/react-query';
+import { User } from '@/interfaces/user';
+import { cn } from '@/lib/utils';
 
 interface Props {
-  image: string;
-  nickname: string;
-  createAt: string;
+  commentId: string;
+  userId: string;
+  createAt: Date;
   content: string;
+  mentionedUser?: string;
+  mentioningUser?: string;
+  className?: string;
 }
 
-export default function Comment({ image, nickname, createAt, content }: Props) {
+export default function Comment({
+  commentId,
+  userId,
+  createAt,
+  content,
+  mentionedUser,
+  mentioningUser,
+  className,
+}: Props) {
   const { data: session } = useSession();
-  const { setMentionedUser, setMentioningUser } = useCommentStore();
+  const { setMentionedUser, setMentioningUser, setCommentId } =
+    useCommentStore();
+
+  const { data: user } = useQuery<User>({
+    queryKey: ['user', userId],
+    queryFn: async () => {
+      const data = await fetch(`/api/user?userId=${userId}`, {
+        method: 'GET',
+      }).then((res) => res.json());
+      return data;
+    },
+  });
 
   const handleNestedComment = () => {
     if (!session) {
@@ -26,14 +52,20 @@ export default function Comment({ image, nickname, createAt, content }: Props) {
       return;
     }
 
-    setMentionedUser(nickname);
+    setMentionedUser(user?.nickname!);
     setMentioningUser(session?.user.nickname!);
+    setCommentId(commentId);
   };
 
   return (
-    <li className="flex space-x-2 items-start border-y py-2 [&:first-child~li]:border-t-0">
+    <li
+      className={cn(
+        'flex space-x-2 items-start border-y py-2 [&:first-child~li]:border-t-0',
+        className
+      )}
+    >
       <Image
-        src={image}
+        src={user?.image ? user.image : '/profile_icon.png'}
         alt="프로필이미지"
         width={40}
         height={40}
@@ -42,10 +74,10 @@ export default function Comment({ image, nickname, createAt, content }: Props) {
       <div className="w-full flex flex-col items-start space-y-2">
         <div className="w-full flex justify-between items-center">
           <div className="flex space-x-2 items-center">
-            <p className="text-sm font-bold">{nickname}</p>
-            <p className="text-xs text-zinc-400">{createAt}</p>
+            <p className="text-sm font-bold">{user?.nickname}</p>
+            <p className="text-xs text-zinc-400">{dateFormatter(createAt)}</p>
           </div>
-          {session && nickname === session.user.nickname && (
+          {session && user?.nickname === session.user.nickname && (
             <div className="flex space-x-2 items-center">
               <Button
                 variant="ghost"
@@ -64,7 +96,13 @@ export default function Comment({ image, nickname, createAt, content }: Props) {
             </div>
           )}
         </div>
-        <p className="text-sm">{content}</p>
+        {mentionedUser ? (
+          <p className="text-sm">
+            <b>@{mentionedUser}</b> {content}
+          </p>
+        ) : (
+          <p className="text-sm">{content}</p>
+        )}
         <Button
           variant="ghost"
           type="button"
