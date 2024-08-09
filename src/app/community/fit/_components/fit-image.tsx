046de12fe664651ba4c6b9df1765a6c37fe5d38e
SeonnHo <prefer914@gmail.com>
2024-08-09 21:01:15 +0900

    fit-image 컴포넌트 신체정보 UI 추가

--- src/app/community/fit/_components/fit-image.tsx
+++ src/app/community/fit/_components/fit-image.tsx
@@ -10,6 +10,7 @@ interface Props {
   fitInfo?: {
     section: string;
     info: string;
+    size: string;
   }[];
   bodyInfo?: {
     gender: string;
@@ -24,6 +25,15 @@ export default function FitImage({
   bodyInfo = { gender: '', height: 0, weight: 0 },
 }: Props) {
   const [isShowInfo, setIsShowInfo] = useState(false);
+
+  const translateGender = (gender: string): string => {
+    const genderMap: Record<string, string> = {
+      male: '남성',
+      female: '여성',
+    };
+
+    return genderMap[gender] || '미기재';
+  };
 
   return (
     <div className="relative w-full h-[500px]">
@@ -40,7 +50,7 @@ export default function FitImage({
               <p className="text-xs">이미지</p>
             </Button>
           </div>
-          {fitInfo ? (
+          {bodyInfo && (
             <div className="p-2 flex flex-col space-y-2">
               <table className="w-full">
                 <tbody>
@@ -54,30 +64,52 @@ export default function FitImage({
                     <th className="text-sm bg-zinc-100 w-1/3 p-2">몸무게</th>
                   </tr>
                   <tr className="border-x border-b text-center">
-                    <td className="border-r text-sm p-2">{bodyInfo.gender}</td>
-                    <td className="border-r text-sm p-2">{bodyInfo.height}</td>
-                    <td className="text-sm p-2">{bodyInfo.weight}</td>
+                    <td className="border-r text-sm p-2">
+                      {translateGender(bodyInfo.gender)}
+                    </td>
+                    <td className="border-r text-sm p-2">
+                      {bodyInfo.height === 0 ? '미기재' : bodyInfo.height}
+                    </td>
+                    <td className="text-sm p-2">
+                      {bodyInfo.weight === 0 ? '미기재' : bodyInfo.weight}
+                    </td>
                   </tr>
                 </tbody>
               </table>
-              <table className="w-full">
-                <tbody>
-                  {fitInfo.map((info) => (
-                    <tr key={info.section} className="border text-center">
-                      <td className="border-r w-1/5 font-bold bg-zinc-100 p-2 text-sm">
-                        {info.section}
-                      </td>
-                      <td className="p-2 text-sm line-clamp-1">{info.info}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          ) : (
-            <div className="flex justify-center items-center flex-grow">
-              <p className="font-bold text-center">
-                해당 핏에 대한 정보가 없습니다.
-              </p>
+              {fitInfo && fitInfo.length > 0 ? (
+                <table className="w-full">
+                  <tbody>
+                    {fitInfo.map((info) => (
+                      <tr
+                        key={info.section}
+                        className="border text-center flex"
+                      >
+                        <td className="border-r w-1/5 font-bold bg-zinc-100 p-2 text-sm line-clamp-1">
+                          <div className="line-clamp-1 break-all text-ellipsis">
+                            {info.section}
+                          </div>
+                        </td>
+                        <td className="w-3/5 p-2 text-sm border-r">
+                          <div className="line-clamp-1 break-all text-ellipsis">
+                            {info.info}
+                          </div>
+                        </td>
+                        <td className="w-1/5 p-2 text-sm line-clamp-1">
+                          <div className="line-clamp-1 break-all text-ellipsis">
+                            {info.size}
+                          </div>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              ) : (
+                <div className="flex justify-center items-center flex-grow">
+                  <p className="font-bold text-center">
+                    해당 핏에 대한 정보가 없습니다.
+                  </p>
+                </div>
+              )}
             </div>
           )}
         </div>
