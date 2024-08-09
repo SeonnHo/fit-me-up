f76e6212712ae84980fd0cbca5ce0a97d3a8f041
SeonnHo <prefer914@gmail.com>
2024-08-09 21:06:07 +0900

    comment api post 요청 시 게시물 댓글 수 업데이트 구현

--- src/app/api/comment/route.ts
+++ src/app/api/comment/route.ts
@@ -3,6 +3,7 @@ import { connectDB } from '@/lib/database';
 import { NextRequest, NextResponse } from 'next/server';
 import { Comment } from '@/interfaces/comment';
 import { getNextSequence } from '@/lib/sequence-counter';
+import { Board } from '@/interfaces/board';
 
 interface RequestBody {
   userId: string;
@@ -39,38 +40,54 @@ export async function GET(request: NextRequest) {
 export async function POST(request: NextRequest) {
   const database = connectDB.db('fit_me_up');
   const commentsCollection = database.collection<Comment>('comments');
+  const boardsCollection = database.collection<Board>('boards');
 
   const date = new Date();
 
   const body: RequestBody = await request.json();
 
-  if (body.mentioningUser) {
-    const replySequence = await getNextSequence(body._id!);
-    const reply = await commentsCollection.updateOne(
-      { _id: new ObjectId(body._id) },
-      {
-        $push: {
-          replies: {
-            id: replySequence,
-            userId: body.userId,
-            boardId: body.boardId,
-            content: body.content,
-            createAt: date,
-            mentionedUser: body.mentionedUser!,
-            mentioningUser: body.mentioningUser!,
+  try {
+    if (body.mentioningUser) {
+      const replySequence = await getNextSequence(body._id!);
+      const reply = await commentsCollection.updateOne(
+        { _id: new ObjectId(body._id) },
+        {
+          $push: {
+            replies: {
+              id: replySequence,
+              userId: body.userId,
+              boardId: body.boardId,
+              content: body.content,
+              createAt: date,
+              mentionedUser: body.mentionedUser!,
+              mentioningUser: body.mentioningUser!,
+            },
           },
-        },
-      }
-    );
-    return NextResponse.json(reply);
-  } else {
-    const comment = await commentsCollection.insertOne({
-      userId: body.userId,
-      boardId: body.boardId,
-      content: body.content,
-      createAt: date,
-    });
+        }
+      );
+      const boardCommentCount = await boardsCollection.findOneAndUpdate(
+        { _id: new ObjectId(body.boardId) },
+        { $inc: { commentCount: 1 } }
+      );
+      console.log(boardCommentCount);
 
-    return NextResponse.json(comment);
+      return NextResponse.json(reply);
+    } else {
+      const comment = await commentsCollection.insertOne({
+        userId: body.userId,
+        boardId: body.boardId,
+        content: body.content,
+        createAt: date,
+      });
+      const boardCommentCount = await boardsCollection.findOneAndUpdate(
+        { _id: new ObjectId(body.boardId) },
+        { $inc: { commentCount: 1 } }
+      );
+      console.log(boardCommentCount);
+
+      return NextResponse.json(comment);
+    }
+  } catch (error) {
+    throw new Error(error as string);
   }
 }
