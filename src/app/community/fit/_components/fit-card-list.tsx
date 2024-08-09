00b2be43773f09e0bea738da4413fd0c9b7ed6e3
SeonnHo <prefer914@gmail.com>
2024-08-09 21:11:25 +0900

    fit-card-list 컴포넌트 useInfiniteQuery를 활용한 무한스크롤 구현

--- src/app/community/fit/_components/fit-card-list.tsx
+++ src/app/community/fit/_components/fit-card-list.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery, useQueries, useQuery } from '@tanstack/react-query';
 import FitCard from './fit-card';
 import { Board } from '@/interfaces/board';
 import { ObjectId } from 'mongodb';
 import { BeatLoader } from 'react-spinners';
+import { useIntersectionObserver } from '@/hooks/use-intersection-observer';
 
 interface TodayFitBoard extends Omit<Board, 'user' | 'fitInfo'> {
   _id: string;
@@ -15,18 +16,37 @@ interface TodayFitBoard extends Omit<Board, 'user' | 'fitInfo'> {
   fitInfo: {
     section: string;
     info: string;
+    size: string;
   }[];
 }
 
+interface TodayFitBoardResponse {
+  boards: TodayFitBoard[];
+  page: number;
+  next: boolean | null;
+}
+
 export default function FitCardList() {
-  const { data, isLoading } = useQuery<TodayFitBoard[]>({
-    queryKey: ['boards', 'todayFit'],
-    queryFn: async () => {
-      const data = await fetch('/api/board?category=todayFit', {
-        method: 'GET',
-      }).then((res) => res.json());
-      return data;
-    },
+  const { data, isLoading, isFetching, hasNextPage, fetchNextPage } =
+    useInfiniteQuery<TodayFitBoardResponse>({
+      queryKey: ['boards', 'todayFit'],
+      queryFn: async ({ pageParam }) => {
+        const data = await fetch(
+          `/api/board?category=todayFit&page=${pageParam}&limit=${6}`,
+          {
+            method: 'GET',
+          }
+        ).then((res) => res.json());
+        return data;
+      },
+      initialPageParam: 1,
+      getNextPageParam: (lastPage) =>
+        lastPage.next !== null ? Number(lastPage.page) + 1 : undefined,
+    });
+
+  const observeTargetRef = useIntersectionObserver({
+    hasNextPage,
+    fetchNextPage,
   });
 
   if (isLoading)
@@ -37,10 +57,21 @@ export default function FitCardList() {
     );
 
   return (
-    <section className="grid grid-cols-3 grid-flow-row gap-4 mb-5 max-xl:grid-cols-2 max-lg:flex max-lg:flex-col">
-      {data!.map((item) => (
-        <FitCard key={item._id as string} {...item} />
-      ))}
-    </section>
+    <>
+      <section className="grid grid-cols-3 grid-flow-row gap-4 mb-5 max-xl:grid-cols-2 max-lg:flex max-lg:flex-col">
+        {data!.pages.flatMap((page) =>
+          page.boards.flatMap((item) => (
+            <FitCard key={item._id as string} {...item} />
+          ))
+        )}
+      </section>
+      {isFetching ? (
+        <div className="flex justify-center items-center">
+          <BeatLoader size={15} color="black" />
+        </div>
+      ) : (
+        <div ref={observeTargetRef} className="h-5" />
+      )}
+    </>
   );
 }
