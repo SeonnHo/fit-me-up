d42eecfc5b05bf65859923661a5818357db02f45
SeonnHo <prefer914@gmail.com>
2024-08-20 18:36:18 +0900

    features의 sign-up sliced의 index파일 export문 추가

--- src/features/sign-up/index.ts
+++ src/features/sign-up/index.ts
@@ -1,1 +1,3 @@
 export { CredentialsSignUpForm } from './ui/credentials-sign-up-form';
+export { OAuthSignUpForm } from './ui/oauth-sign-up-form';
+export { useValidationStore } from './model/validation-store';
