b5eef20295f637e83bfb0e70cc1a0718dfbbeabe
SeonnHo <prefer914@gmail.com>
2024-08-21 23:27:08 +0900

    사용자 엔티티 분리

--- src/entities/user/index.ts
+++ src/entities/user/index.ts
@@ -1,6 +1,2 @@
-export {
-  checkEmailDuplication,
-  checkNicknameDuplication,
-} from './api/check-duplication';
 export { useUserQuery } from './api/user-query';
 export type { User } from './lib/user-interface';
--- src/entities/user/lib/user-interface.ts
+++ src/entities/user/lib/user-interface.ts
@@ -3,7 +3,6 @@ import { ObjectId } from 'mongodb';
 export interface User {
   _id: string | ObjectId;
   email: string | null | undefined;
-  password: string | null | undefined;
   image: string | null | undefined;
   nickname: string | null | undefined;
   type: string;
