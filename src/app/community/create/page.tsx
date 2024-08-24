1d40de2865a01b3f21b28450b0cc6febcd920b3d
SeonnHo <prefer914@gmail.com>
2024-08-24 21:56:11 +0900

    게시물 생성 페이지 수정

--- src/app/community/create/page.tsx
+++ src/app/community/create/page.tsx
@@ -1,11 +1,16 @@
-import CategoryImagePicker from './_components/category-image-picker';
-import BoardCreationForm from './_components/board-creation-form';
+import { PostForm } from '@/wigets/post-form';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '핏미업 - 게시물 생성',
+  description:
+    '핏미업 게시물 생성 페이지입니다. 핏미업에서 여러 사람들과 패션에 관해 정보도 나누고 소통해요!',
+};
 
 export default function CommunityCreatePage() {
   return (
     <main className="flex flex-col w-full max-w-screen-sm">
-      <CategoryImagePicker />
-      <BoardCreationForm />
+      <PostForm />
     </main>
   );
 }
