60d41d227989176a78175860d0c60ac4dce92d01
SeonnHo <prefer914@gmail.com>
2024-09-03 23:28:55 +0900

    validation store 이메일 유효성 관련 상태 추가

--- src/features/sign-up/model/validation-store.ts
+++ src/features/sign-up/model/validation-store.ts
@@ -3,9 +3,13 @@ import { create } from 'zustand';
 interface ValidationStore {
   isValidNickname: boolean;
   setIsValidNickname: (value: boolean) => void;
+  isValidEmail: boolean;
+  setIsValidEmail: (value: boolean) => void;
 }
 
 export const useValidationStore = create<ValidationStore>((set) => ({
   isValidNickname: false,
   setIsValidNickname: (value) => set({ isValidNickname: value }),
+  isValidEmail: false,
+  setIsValidEmail: (value) => set({ isValidEmail: value }),
 }));
