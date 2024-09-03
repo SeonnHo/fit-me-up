b5052679110cf96b2cd3700fb7ac5a65eab5a22e
SeonnHo <prefer914@gmail.com>
2024-09-03 23:32:22 +0900

    oauth sign up card 컴포넌트 session 로딩 여부에 따른 로딩 UI 표시 추가 및 사용자 회원가입 필요 여부 확인 로직 추가

--- src/wigets/oauth-sign-up-card/ui/oauth-sign-up-card.tsx
+++ src/wigets/oauth-sign-up-card/ui/oauth-sign-up-card.tsx
@@ -2,18 +2,29 @@
 
 import { OAuthSignUpForm, useValidationStore } from '@/features/sign-up';
 import { Card, CardContent, CardHeader, CardTitle } from '@/shared/ui/card';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 import { RiErrorWarningFill } from 'react-icons/ri';
+import { BeatLoader } from 'react-spinners';
 
-interface OAuthSignUpCardProps {
-  oauthId: string;
-  provider: string;
-}
+export const OAuthSignUpCard = () => {
+  const { isValidNickname, isValidEmail } = useValidationStore();
+  const { data: session, status } = useSession();
+  const router = useRouter();
 
-export const OAuthSignUpCard = ({
-  oauthId,
-  provider,
-}: OAuthSignUpCardProps) => {
-  const { isValidNickname } = useValidationStore();
+  useEffect(() => {
+    if (session && !session.user.needsSignUp) {
+      router.replace('/');
+    }
+  }, [session, router]);
+
+  if (status === 'loading')
+    return (
+      <section>
+        <BeatLoader size={15} />
+      </section>
+    );
 
   return (
     <Card className="w-[500px] max-sm:w-full max-sm:border-none max-sm:shadow-none relative">
@@ -21,15 +32,18 @@ export const OAuthSignUpCard = ({
         <CardTitle className="text-center">회원가입</CardTitle>
       </CardHeader>
       <CardContent>
-        <OAuthSignUpForm oauthId={oauthId} provider={provider} />
+        <OAuthSignUpForm
+          oauthId={session?.user.oauthId!}
+          provider={session?.user.oauthProvider!}
+          defaultEmail={session?.user.email}
+          defaultNickname={session?.user.nickname}
+        />
       </CardContent>
-      {!isValidNickname && (
+      {(!isValidNickname || !isValidEmail) && (
         <section className="flex justify-center items-center mt-4 max-sm:flex-col max-sm:px-2 absolute -bottom-10 left-1/2 -translate-x-1/2 w-screen max-sm:-bottom-14">
           <RiErrorWarningFill className="size-5 text-red-600 mr-2 max-sm:mb-2 shrink-0" />
           <p className="font-bold text-sm text-red-600 text-center">
-            커뮤니티를 이용하기 위해 닉네임이 필요합니다.&nbsp;
-            <br className="sm:hidden" />
-            닉네임을 정하여 회원가입을 완료해주세요.
+            커뮤니티를 이용하기 위해 이메일과 닉네임이 필요합니다.
           </p>
         </section>
       )}
