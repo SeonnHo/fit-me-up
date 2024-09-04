57fd1c11db2d2b4f203b1bf25ccb13bdea36f9a2
SeonnHo <prefer914@gmail.com>
2024-09-04 23:57:13 +0900

    comment store 멘션한사용자 닉네임 전역 상태 및 상태 관리 함수 추가

--- src/entities/comment/model/comment-store.ts
+++ src/entities/comment/model/comment-store.ts
@@ -2,18 +2,23 @@ import { create } from 'zustand';
 
 interface CommentStore {
   commentId: string;
-  mentionedUser: string;
-  mentioningUser: string;
   setCommentId: (id: string) => void;
-  setMentionedUser: (user: string) => void;
-  setMentioningUser: (user: string) => void;
+  mentionedUserId: string;
+  setMentionedUserId: (userId: string) => void;
+  mentioningUserId: string;
+  setMentioningUserId: (userId: string) => void;
+  mentionedUserNickname: string;
+  setMentionedUserNickname: (nickname: string) => void;
 }
 
 export const useCommentStore = create<CommentStore>((set) => ({
   commentId: '',
-  mentionedUser: '',
-  mentioningUser: '',
   setCommentId: (id) => set({ commentId: id }),
-  setMentionedUser: (user) => set({ mentionedUser: user }),
-  setMentioningUser: (user) => set({ mentioningUser: user }),
+  mentionedUserId: '',
+  setMentionedUserId: (userId) => set({ mentionedUserId: userId }),
+  mentioningUserId: '',
+  setMentioningUserId: (userId) => set({ mentioningUserId: userId }),
+  mentionedUserNickname: '',
+  setMentionedUserNickname: (nickname) =>
+    set({ mentionedUserNickname: nickname }),
 }));
