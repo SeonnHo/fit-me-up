4ee063c7f0773f36c3985ce6fd3ab3f22696c7ce
SeonnHo <prefer914@gmail.com>
2024-09-07 23:36:10 +0900

    today fit 페이지 검색란 스타일 수정

--- src/app/community/todayfit/page.tsx
+++ src/app/community/todayfit/page.tsx
@@ -13,11 +13,11 @@ export const metadata: Metadata = {
 export default function TodayFitPage() {
   return (
     <main className="flex flex-col max-sm:w-full">
-      <section className="flex justify-end items-center mb-4 space-x-2">
+      <section className="flex justify-end max-sm:justify-center items-center mb-4 space-x-2 max-sm:px-2">
         <SearchInput />
         <Link
           href="/community/create"
-          className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground w-10 h-10"
+          className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground w-10 h-10 shrink-0"
         >
           <FaRegEdit className="size-5" />
         </Link>
