a8b3da7fb37f63444ba2d8fe60b68fd6338f3239
SeonnHo <prefer914@gmail.com>
2024-09-07 23:41:54 +0900

    skeleton comment item 컴포넌트 스타일 수정

--- src/wigets/comment-card/ui/skeleton-comment-item.tsx
+++ src/wigets/comment-card/ui/skeleton-comment-item.tsx
@@ -2,12 +2,12 @@ import { Skeleton } from '@/shared/ui/skeleton';
 
 export const SkeletonCommentItem = () => {
   return (
-    <li className="flex space-x-2 items-start py-2">
-      <Skeleton className="rounded-full h-10 w-10" />
+    <li className="w-full flex space-x-2 items-start py-2">
+      <Skeleton className="rounded-full h-10 w-10 shrink-0" />
       <div className="w-full flex flex-col items-start space-y-2">
-        <Skeleton className="h-[14px] w-10" />
-        <Skeleton className="h-[14px] w-20" />
-        <Skeleton className="h-[14px] w-5" />
+        <Skeleton className="h-[20px] w-[100px]" />
+        <Skeleton className="h-[20px] w-[200px]" />
+        <Skeleton className="h-[16px] w-[50px]" />
       </div>
     </li>
   );
