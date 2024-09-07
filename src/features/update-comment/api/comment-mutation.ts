76caff7ccf719e8db069c8f42159fd6ea84243e2
SeonnHo <prefer914@gmail.com>
2024-09-07 23:48:47 +0900

    comment mutation 댓글 수정 및  삭제 API 요청 로직 추가

--- src/features/update-comment/api/comment-mutation.ts
+++ src/features/update-comment/api/comment-mutation.ts
@@ -1,44 +1,70 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 interface UseCommentMutationProps {
-  commentId: string;
-  userId: string;
-  postId: string;
-  content: string;
-  mentionedUserId: string;
-  mentioningUserId: string;
-  category: string;
+  commentId?: string;
+  parentCommentId?: string;
+  userId?: string;
+  postId?: string;
+  content?: string;
+  mentionedUserId?: string;
+  mentioningUserId?: string;
+  category?: string;
 }
 
-export const useCommentMutation = () => {
+export const useCommentMutation = ({
+  method,
+}: {
+  method: 'POST' | 'PUT' | 'DELETE';
+}) => {
   const queryClient = useQueryClient();
 
-  const addComment = async ({
+  const updateComment = async ({
     commentId,
+    parentCommentId,
     userId,
     postId,
     content,
     mentionedUserId,
     mentioningUserId,
   }: UseCommentMutationProps) => {
+    const getRequestBody = (method: 'POST' | 'PUT' | 'DELETE') => {
+      switch (method) {
+        case 'POST':
+          return JSON.stringify({
+            parentCommentId,
+            userId,
+            postId,
+            content,
+            mentionedUserId,
+            mentioningUserId,
+          });
+
+        case 'PUT':
+          return JSON.stringify({
+            commentId,
+            content,
+          });
+
+        case 'DELETE':
+          return JSON.stringify({
+            commentId,
+          });
+        default:
+          throw new Error('올바르지 않은 method입니다.');
+      }
+    };
+
     const res = await fetch('/api/comment', {
-      method: 'POST',
+      method,
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        commentId: commentId,
-        userId: userId,
-        postId: postId,
-        content: content,
-        mentionedUserId,
-        mentioningUserId,
-      }),
+      body: getRequestBody(method),
     });
 
     return res.json();
   };
 
   const { mutate } = useMutation({
-    mutationFn: addComment,
+    mutationFn: updateComment,
     onSuccess: (_, { postId, category }) => {
       queryClient.invalidateQueries({ queryKey: ['comments', postId] });
       queryClient.invalidateQueries({
