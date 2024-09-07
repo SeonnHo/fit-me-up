d91368bbe391020894dd71dbfb7258bd6ff60b5a
SeonnHo <prefer914@gmail.com>
2024-09-07 23:34:30 +0900

    prisma client의 타입 재사용으로 인한 도메인별 인터페이스 삭제

--- src/entities/comment/lib/reply-interface.ts
+++
@@ -1,7 +0,0 @@
-import { Comment } from './comment-interface';
-
-export interface Reply extends Omit<Comment, 'replies' | '_id'> {
-  id: number;
-  mentionedUser: string;
-  mentioningUser: string;
-}
