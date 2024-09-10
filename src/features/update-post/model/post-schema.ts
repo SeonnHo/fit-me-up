5cfa319465df3f8308c9361b3297e5565616a375
SeonnHo <prefer914@gmail.com>
2024-09-10 19:00:08 +0900

    post schema 수정

--- src/features/update-post/model/post-schema.ts
+++ src/features/update-post/model/post-schema.ts
@@ -1,30 +1,5 @@
 import { z } from 'zod';
 
-const baseSchema = {
-  category: z.union([
-    z.literal('todayFit'),
-    z.literal('manFree'),
-    z.literal('manQuestion'),
-    z.literal('manInfo'),
-    z.literal('womanFree'),
-    z.literal('womanQuestion'),
-    z.literal('womanInfo'),
-  ]),
-  content: z.string().min(1, { message: '내용은 필수 입력 요소입니다.' }),
-  bodyInfo: z.object({
-    gender: z.union([z.literal('male'), z.literal('female')]).optional(),
-    height: z.coerce.number().min(0).max(300).optional(),
-    weight: z.coerce.number().min(0).max(300).optional(),
-  }),
-  fashionInfo: z.array(
-    z.object({
-      section: z.string().min(1),
-      info: z.string().min(1),
-      size: z.string().min(1),
-    })
-  ),
-};
-
 const fileSchema = z.object({
   name: z.string(),
   type: z.string().refine(
@@ -44,27 +19,39 @@ const fileSchema = z.object({
     .max(5 * 1024 * 1024, { message: '파일 크기는 5MB를 초과할 수 없습니다.' }),
 });
 
-export const postFormSchema = z.union([
+export const postFormSchema = z.discriminatedUnion('category', [
   z.object({
-    ...baseSchema,
     category: z.literal('todayFit'),
     title: z.string().optional(),
+    content: z.string().min(1, { message: '내용은 필수 입력 요소입니다.' }),
+    bodyInfo: z.object({
+      gender: z.union([z.literal('male'), z.literal('female')]).optional(),
+      height: z.coerce.number().min(0).max(300).optional(),
+      weight: z.coerce.number().min(0).max(300).optional(),
+    }),
+    fashionInfo: z.array(
+      z.object({
+        section: z.string().min(1),
+        info: z.string().min(1),
+        size: z.string().min(1),
+      })
+    ),
     files: z
       .array(fileSchema)
       .min(1, { message: '최소 1개의 파일이 필요합니다.' })
       .max(1, { message: '최대 1개의 파일만 허용됩니다.' }),
   }),
   z.object({
-    ...baseSchema,
     category: z.enum([
-      'manFree',
-      'manQuestion',
-      'manInfo',
-      'womanFree',
-      'womanQuestion',
-      'womanInfo',
+      'maleFree',
+      'maleQuestion',
+      'maleInfo',
+      'femaleFree',
+      'femaleQuestion',
+      'femaleInfo',
     ]),
     title: z.string().min(1, { message: '제목은 필수 입력 요소입니다.' }),
+    content: z.string().min(1, { message: '내용은 필수 입력 요소입니다.' }),
     files: z.array(fileSchema).optional(),
   }),
 ]);
