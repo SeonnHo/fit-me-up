4e0e4ac86dd50bb0bbef29b7a19586b62749ac96
SeonnHo <prefer914@gmail.com>
2024-09-25 18:46:47 +0900

    커뮤니티 페이지 기존 네비게이션 UI 로직 삭제 및 전체 글 테이블 UI 구현

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -1,44 +1,32 @@
+import { SearchInput } from '@/features/search';
 import { NavigationCard } from '@/wigets/navigation-card';
+import { PostsTable } from '@/wigets/posts-table';
+import { Metadata } from 'next';
+import Link from 'next/link';
 import React from 'react';
+import { FaRegEdit } from 'react-icons/fa';
+
+export const metadata: Metadata = {
+  title: '핏미업 - 커뮤니티',
+  description: '핏미업 커뮤니티 페이지입니다.',
+};
 
 export default function CommunityPage() {
-  const navCategoryList = [
-    { name: '전체 글', path: '/community' },
-    { name: '남성 커뮤니티', path: '/community/male' },
-    { name: '여성 커뮤니티', path: '/community/female' },
-  ];
-
-  const boardList = [
-    { name: '자유게시판', path: '/free' },
-    { name: '질문게시판', path: '/question' },
-    { name: '정보게시판', path: '/info' },
-  ];
   return (
-    <main className="relative max-w-screen-xl h-[300vh] flex lg:items-start lg:space-x-4">
+    <main className="flex space-x-5">
       <NavigationCard />
-      <nav className="sticky top-[100px] border rounded w-[200px] p-4 max-lg:hidden">
-        <ul>
-          {navCategoryList.map((category) => (
-            <React.Fragment key={category.name}>
-              <li className="text-sm [&~&]:mt-4 cursor-pointer hover:font-bold">
-                {category.name}
-              </li>
-              {category.name !== '전체 글' && (
-                <ul className="space-y-2 mt-2 px-3">
-                  {boardList.map((board) => (
-                    <li
-                      key={board.name}
-                      className="text-sm cursor-pointer hover:font-bold"
-                    >
-                      {board.name}
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </React.Fragment>
-          ))}
-        </ul>
-      </nav>
+      <div className="flex flex-col space-y-2">
+        <div className="flex justify-end items-center space-x-2">
+          <SearchInput />
+          <Link
+            href="/community/create"
+            className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground w-10 h-10 shrink-0"
+          >
+            <FaRegEdit className="size-5" />
+          </Link>
+        </div>
+        <PostsTable category="all" />
+      </div>
     </main>
   );
 }
