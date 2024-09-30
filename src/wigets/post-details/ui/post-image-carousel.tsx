4d430c7686207d3d375ab6435932019418db3b4d
SeonnHo <prefer914@gmail.com>
2024-09-30 21:20:36 +0900

    post image carousel 컴포넌트 스타일 수정

--- src/wigets/post-details/ui/post-image-carousel.tsx
+++ src/wigets/post-details/ui/post-image-carousel.tsx
@@ -32,11 +32,14 @@ export const PostImageCarousel = ({ imageUrls }: PostImageCarouselProps) => {
 
   return (
     <div className="w-full flex flex-col justify-center items-center space-y-4">
-      <Carousel className="xl:w-[600px] lg:w-[500px] w-[300px]" setApi={setApi}>
+      <Carousel
+        className="xl:w-[600px] lg:w-[500px] md:w-[400px] w-[300px]"
+        setApi={setApi}
+      >
         <CarouselContent>
           {imageUrls.map((url, index) => (
             <CarouselItem key={url}>
-              <div className="relative rounded-md border xl:size-[600px] lg:size-[500px] size-[300px]">
+              <div className="relative rounded-md border xl:size-[600px] lg:size-[500px] md:size-[400px] size-[300px]">
                 <Image
                   src={url}
                   alt={`게시물 이미지 ${index}`}
