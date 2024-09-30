0af7fc7fd790c4f91d63ef0e5330a69fb8a678fc
SeonnHo <prefer914@gmail.com>
2024-09-30 21:21:24 +0900

    url 경로에 따른 게시물 카테고리 정보 UI 표시 로직 추가

--- src/wigets/posts-table/ui/post-table-row.tsx
+++ src/wigets/posts-table/ui/post-table-row.tsx
@@ -5,7 +5,7 @@ import { Prisma } from '@prisma/client';
 import { FaImage } from 'react-icons/fa6';
 import { format, isToday } from 'date-fns';
 import { ko } from 'date-fns/locale';
-import { useRouter } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import { Separator } from '@/shared/ui/separator';
 
 type PostSummary = Prisma.PostGetPayload<{
@@ -51,6 +51,29 @@ export const PostTableRow = ({
   isTablet,
 }: PostsTableRowProps) => {
   const router = useRouter();
+  const pathname = usePathname();
+
+  const formatCategoryName = (category: string, pathname: string) => {
+    const categoryMap = {
+      maleFree: '자유게시판',
+      maleInfo: '정보게시판',
+      maleQuestion: '질문게시판',
+      femaleFree: '자유게시판',
+      femaleInfo: '정보게시판',
+      femaleQuestion: '질문게시판',
+    };
+
+    const genderPrefix =
+      pathname === '/community'
+        ? category.startsWith('male')
+          ? '남성 '
+          : '여성 '
+        : '';
+
+    return (
+      genderPrefix + (categoryMap[category as keyof typeof categoryMap] || '')
+    );
+  };
 
   return (
     <TableRow
@@ -83,6 +106,16 @@ export const PostTableRow = ({
               <span className="text-xs text-muted-foreground">
                 조회 {post.viewCount}
               </span>
+              {['/community', '/community/male', '/community/female'].includes(
+                pathname
+              ) && (
+                <>
+                  <Separator orientation="vertical" className="h-4" />
+                  <span className="text-xs text-muted-foreground">
+                    {formatCategoryName(post.category, pathname)}
+                  </span>
+                </>
+              )}
             </div>
           </TableCell>
           <TableCell className="text-red-500 p-2">
@@ -91,26 +124,37 @@ export const PostTableRow = ({
         </>
       ) : (
         <>
-          <TableCell className="flex items-center space-x-1 text-center p-2 xl:w-[600px]">
-            <span className="line-clamp-1 break-all">{post.title}</span>
-            {post.imageUrls.length > 0 && (
-              <span className="shrink-0">
-                <FaImage className="w-3 h-3 text-muted-foreground" />
-              </span>
-            )}
-            {post._count.comments > 0 && (
-              <span className="text-red-600 font-bold shrink-0">
-                &#91;{post._count.comments}&#93;
-              </span>
-            )}
+          {['/community', '/community/male', '/community/female'].includes(
+            pathname
+          ) && (
+            <TableCell className="text-muted-foreground text-sm p-2 text-nowrap">
+              {formatCategoryName(post.category, pathname)}
+            </TableCell>
+          )}
+          <TableCell className="p-2 xl:w-[600px]">
+            <div className="flex items-center space-x-1">
+              <span className="line-clamp-1 break-all">{post.title}</span>
+              {post.imageUrls.length > 0 && (
+                <span className="shrink-0">
+                  <FaImage className="w-3 h-3 text-muted-foreground" />
+                </span>
+              )}
+              {post._count.comments > 0 && (
+                <span className="text-red-600 font-bold shrink-0">
+                  &#91;{post._count.comments}&#93;
+                </span>
+              )}
+            </div>
           </TableCell>
-          <TableCell className="text-center p-2 w-[100px]">
+          <TableCell className="text-center p-2 w-[100px] text-nowrap">
             {post.author.nickname}
           </TableCell>
-          <TableCell className="text-center p-2">
+          <TableCell className="text-center p-2 text-nowrap">
             {formatDate(post.createdAt)}
           </TableCell>
-          <TableCell className="text-center p-2">{post.viewCount}</TableCell>
+          <TableCell className="text-center p-2 text-nowrap">
+            {post.viewCount}
+          </TableCell>
         </>
       )}
     </TableRow>
